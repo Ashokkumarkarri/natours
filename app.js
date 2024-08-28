c99744bc79cad7c00cb79969fdf803f8a88bcbdb
Ashok kumar karri <ashok.ashok1999@gmail.com>
2024-08-28 19:51:31 +0530

    added post method

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const express = require('express');
 const app = express();
+app.use(express.json()); //middleware
 
 // app.get('/', (req, res) => {
 //   res.status(200).json({
@@ -22,6 +23,23 @@ app.get('/api/v1/tours', (req, res) => {
     result: tours.length,
     data: {
       tours: tours,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1; //Last object in array's ID +1
+  const newTour = Object.assign({ id: newId }, req.body); //object.assign will combine two obj together.
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {}
+  );
+  res.status(201).json({
+    status: 'success',
+    data: {
+      tours: newTour,
     },
   });
 });
