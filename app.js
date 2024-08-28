1639e87858d1a9f1679657a15d46c614d7f4e5fb
Ashok kumar karri <ashok.ashok1999@gmail.com>
2024-08-28 18:57:06 +0530

    implemented get request to app.js

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  // res.status(200).send('Hello from the server side!');
-  res.status(200).json({
-    message: 'Hello from the server side',
-    app: 'Natours',
-  });
-});
+// app.get('/', (req, res) => {
+//   res.status(200).json({
+//     message: 'Hello from the server side',
+//     app: 'Natours',
+//   });
+// });
 
-app.post('/', (req, res) => {
-  res.send('posting');
+// app.post('/', (req, res) => {
+//   res.send('posting');
+// });
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    result: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
 });
 
 //To start the server
