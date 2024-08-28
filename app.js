36aee589af241920e5a24808972643000f3fb12b
Ashok kumar karri <ashok.ashok1999@gmail.com>
2024-08-28 11:41:45 +0530

    basic routing

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  // res.status(200).send('Hello from the server side!');
+  res.status(200).json({
+    message: 'Hello from the server side',
+    app: 'Natours',
+  });
+});
+
+app.post('/', (req, res) => {
+  res.send('posting');
+});
+
+//To start the server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App runing on ${port}....`);
+});
