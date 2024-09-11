544440f269852850c5a9dcfb5003b1505ef9d971
Ashok kumar karri <ashok.ashok1999@gmail.com>
2024-09-12 01:46:28 +0530

    created Simple Tour Model

--- server.js
+++ server.js
@@ -13,6 +13,30 @@ mongoose.connect(DB).then(() => {
   console.log('DB connection successful');
 });
 
+//-------------------------------------------------------------------------------------------------------------
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.1,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a Price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+//name of the model  , schema
+//allways use upper case ofr model names and varible.
+// thats why I had used Captial T for the varible so that we will get to know that we are dealing with mode.
+
+//-------------------------------------------------------------------------------------------------------------
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`App runing on ${port}....`);
