80ba50515bca2de560f41c4151cef7717239e927
Ashok kumar karri <ashok.ashok1999@gmail.com>
2024-09-17 05:09:00 +0530

    updating Document

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -57,19 +57,24 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
-  // if (req.params.id * 1 > tours.length) {
-  //   return res.status(404).json({
-  //     status: 'fail',
-  //     message: 'Invalid ID',
-  //   });
-  // }
-  res.status(200).json({
-    status: 'Success',
-    data: {
-      tour: '<Updated tour is here>',
-    },
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({
+      status: 'Success',
+      data: {
+        tour: tour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
 exports.deleteTour = (req, res) => {
