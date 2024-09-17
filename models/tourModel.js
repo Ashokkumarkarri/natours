4df46404134e339964e5384a793273546bbff00d
Ashok kumar karri <ashok.ashok1999@gmail.com>
2024-09-17 06:08:27 +0530

    Modelling Tours

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,18 +1,61 @@
 const mongoose = require('mongoose');
+
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'A tour must have a name'],
     unique: true,
+    trim: true,
+    //trim removes all the black space in the begining and ending.
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+
+  ratingsAverage: {
     type: Number,
     default: 4.1,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+    //it will be calculated from our code, it will check how many ratings are then and then it will calculate.
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a Price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    //trim removes all the black space in the begining and ending.
+    required: [true, 'A Tour must have a summary'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A Tour must have a cover image'],
+    //now we are just storing the name of the image, we will store the real image from file system later.
+  },
+  images: [String], //we have an array of strings,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
