48cfe0bbb4419cb69f2baa93c5badcce83678f42
Ashok kumar karri <ashok.ashok1999@gmail.com>
2024-10-01 00:40:16 +0530

    Importing Development Data

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('./../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<db_password>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose.connect(DB).then(() => {
+  console.log('DB connection successful');
+});
+
+//reading JSON file
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'),
+);
+
+//Import data into DB
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded');
+    process.exit();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+//Delete all Data from collection
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data successfully Deleted');
+    process.exit(); //this method will forcefull stop the process
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
+
+console.log(process.argv);
