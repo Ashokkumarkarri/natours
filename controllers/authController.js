259220ea3978b4ea082508fce55a65dcc8d7ddb4
Ashok kumar karri <ashok.ashok1999@gmail.com>
2025-02-17 22:15:54 +0530

    007 Testing for Secure HTTPS Connections

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,10 +12,15 @@ const signToken = (id) => {
   });
 };
 //function to send responses
-const createSendToken = (user, statusCode, res) => {
+const createSendToken = (user, statusCode, req, res) => {
   const token = signToken(user._id);
   // Set cookie options
-  const cookieOptions = {
+
+  // Only send cookies securely in production (HTTPS)
+
+  // Send the cookie to the client
+  //arg1=name of the cookie, arg2=data we want to send, aeg3=some options
+  res.cookie('jwt', token, {
     // Expiration for the cookie
     maxAge: new Date(
       Date.now() +
@@ -23,15 +28,8 @@ const createSendToken = (user, statusCode, res) => {
     ),
     // secure: true, //https
     httpOnly: true, // Prevents client-side scripts from accessing cookies (XSS protection)
-  };
-
-  // Only send cookies securely in production (HTTPS)
-  if (process.env.NODE_ENV === 'production') {
-    cookieOptions.secure = true; // Send cookies only over HTTPS
-  }
-  // Send the cookie to the client
-  //arg1=name of the cookie, arg2=data we want to send, aeg3=some options
-  res.cookie('jwt', token, cookieOptions);
+    secure: req.secure || req.headers('x-forwarded-proto') === 'https', // Send cookies only over HTTPS
+  });
 
   // Remove the password before sending the user data in the response
   user.password = undefined;
@@ -55,7 +53,7 @@ exports.signup = catchAsync(async (req, res, next) => {
   const url = `${req.protocol}://${req.get('host')}/me`;
   // console.log(url);
   await new Email(newUser, url).sendWelcome();
-  createSendToken(newUser, 201, res);
+  createSendToken(newUser, 201, req, res);
 });
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -79,7 +77,7 @@ exports.login = catchAsync(async (req, res, next) => {
     return next(new AppError('Incorrect email or password', 401));
   }
   //3)if everything is okay, send token to client
-  createSendToken(user, 200, res);
+  createSendToken(user, 200, req, res);
 });
 
 exports.logout = (req, res) => {
@@ -239,7 +237,7 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
   //3) Update changedPasswordAt property for the user.
 
   //4)Log the user in, send JWT to client.
-  createSendToken(user, 200, res);
+  createSendToken(user, 200, req, res);
 });
 
 exports.updatePassword = catchAsync(async (req, res, next) => {
@@ -259,5 +257,5 @@ exports.updatePassword = catchAsync(async (req, res, next) => {
 
   //4)Log user in, send JWT
 
-  createSendToken(user, 200, res);
+  createSendToken(user, 200, req, res);
 });
